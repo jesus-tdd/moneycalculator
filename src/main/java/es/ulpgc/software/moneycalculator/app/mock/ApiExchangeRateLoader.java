3d10c5e7c1d2abf8f0a1622774c38b1e4b30cee8
Jesus <jesus.tdd03@gmail.com>
2025-11-19 12:39:12 +0000

    Implemented class ApiExchangeRateLoader

---
+++ src/main/java/es/ulpgc/software/moneycalculator/app/mock/ApiExchangeRateLoader.java
@@ -0,0 +1,55 @@
+package es.ulpgc.software.moneycalculator.app.mock;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import es.ulpgc.software.moneycalculator.io.ExchangeRateLoader;
+import es.ulpgc.software.moneycalculator.model.Currency;
+import es.ulpgc.software.moneycalculator.model.ExchangeRate;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.LocalDate;
+
+public class ApiExchangeRateLoader implements ExchangeRateLoader {
+    private final Api api;
+
+    public ApiExchangeRateLoader(Api api) {
+        this.api = api;
+    }
+
+    @Override
+    public ExchangeRate load(Currency from, Currency to) {
+        try {
+            return new ExchangeRate(
+                    LocalDate.now(),
+                    from,
+                    to,
+                    readConversionRate(new URL(api.url() + "pair/" + from.code() + "/" + to.code()))
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private double readConversionRate(URL url) throws IOException {
+        return readConversionRate(url.openConnection());
+    }
+
+    private double readConversionRate(URLConnection connection) throws IOException {
+        try (InputStream is = connection.getInputStream()) {
+            return readConversionRate(new String(new BufferedInputStream(is).readAllBytes()));
+        }
+    }
+
+    private double readConversionRate(String json) {
+        return readConversionRate(new Gson().fromJson(json, JsonObject.class));
+    }
+
+    private double readConversionRate(JsonObject jsonObject) {
+        return jsonObject.get("conversion_rate").getAsDouble();
+    }
+}
--- src/main/java/es/ulpgc/software/moneycalculator/app/mock/Main.java
+++ src/main/java/es/ulpgc/software/moneycalculator/app/mock/Main.java
@@ -1,14 +1,16 @@
 package es.ulpgc.software.moneycalculator.app.mock;
 
+import es.ulpgc.software.moneycalculator.io.ExchangeRateLoader;
 import es.ulpgc.software.moneycalculator.model.Currency;
+import es.ulpgc.software.moneycalculator.model.ExchangeRate;
 
 import java.util.List;
 
 public class Main {
     static void main() {
-        List<Currency> currencies = new ApiCurrencyLoader().loadAll();
-        for (Currency currency : currencies) {
-            System.out.println(currency);
-        }
+        Api api = new Api();
+        List<Currency> currencies = new ApiCurrencyLoader(api).loadAll();
+        ExchangeRate rate = new ApiExchangeRateLoader(api).load(currencies.get(0), currencies.get(1));
+        System.out.println(rate);
     }
 }
