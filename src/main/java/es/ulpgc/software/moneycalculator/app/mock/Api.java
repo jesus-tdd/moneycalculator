5dd2ed41145edf768fdc9d9b496d535bee281065
Jesus <jesus.tdd03@gmail.com>
2025-11-19 12:24:50 +0000

    Created class Api

---
+++ src/main/java/es/ulpgc/software/moneycalculator/app/mock/Api.java
@@ -0,0 +1,17 @@
+package es.ulpgc.software.moneycalculator.app.mock;
+
+import io.github.cdimascio.dotenv.DotenvBuilder;
+
+public class Api {
+    private final String ApiKey;
+    private final String ApiUrl;
+
+    public Api() {
+        ApiKey = new DotenvBuilder().load().get("API_KEY");
+        ApiUrl = "https://v6.exchangerate-api.com/v6/API-KEY/".replace("API-KEY", this.ApiKey);
+    }
+
+    public String url() {
+        return ApiUrl;
+    }
+}
