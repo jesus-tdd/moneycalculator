803df5cfe603cc8eb96b254cc999e4b4937635b2
Jesus <jesus.tdd03@gmail.com>
2025-11-29 12:52:12 +0000

    Implemented ExchangeCommand

--- src/main/java/es/ulpgc/software/moneycalculator/architecture/control/ExchangeCommand.java
+++ src/main/java/es/ulpgc/software/moneycalculator/architecture/control/ExchangeCommand.java
@@ -1,7 +1,41 @@
 package es.ulpgc.software.moneycalculator.architecture.control;
 
+import es.ulpgc.software.moneycalculator.architecture.io.ExchangeRateLoader;
+import es.ulpgc.software.moneycalculator.architecture.model.Currency;
+import es.ulpgc.software.moneycalculator.architecture.model.ExchangeRate;
+import es.ulpgc.software.moneycalculator.architecture.model.Money;
+import es.ulpgc.software.moneycalculator.architecture.view.CurrencyDialog;
+import es.ulpgc.software.moneycalculator.architecture.view.MoneyDialog;
+import es.ulpgc.software.moneycalculator.architecture.view.MoneyDisplay;
+
+import java.time.LocalDate;
+
 public class ExchangeCommand implements Command{
+    private final MoneyDialog moneyDialog;
+    private final CurrencyDialog currencyDialog;
+    private final MoneyDisplay moneyDisplay;
+    private final ExchangeRateLoader loader;
+
+    public ExchangeCommand(
+            MoneyDialog moneyDialog,
+            CurrencyDialog currencyDialog,
+            MoneyDisplay moneyDisplay,
+            ExchangeRateLoader loader
+    ) {
+        this.moneyDialog = moneyDialog;
+        this.currencyDialog = currencyDialog;
+        this.moneyDisplay = moneyDisplay;
+        this.loader = loader;
+    }
+
     @Override
     public void execute() {
+        Money money = moneyDialog.get();
+        Currency currency = currencyDialog.get();
+
+        ExchangeRate exchangeRate = loader.load(money.currency(), currency);
+        Money result = new Money(money.amount() * exchangeRate.rate(), currency);
+
+        moneyDisplay.show(result);
     }
 }
